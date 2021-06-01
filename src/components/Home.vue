bb7158982217f1bb0da5cb418521e148b73a967b
PeterYangs <904801074@qq.com>
2021-06-01 14:08:40 +0800

    globalProperties

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -10,6 +10,9 @@ export default {
   created() {
 
 
+    this.$AppContext.test();
+
+
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from "./vuex";
 import ElementPlus from 'element-plus';
 import 'element-plus/lib/theme-chalk/index.css';
 
 
-createApp(App).use(router).use(ElementPlus).mount('#app')
+const app=createApp(App)
+
+
+
+
+app.config.globalProperties.$app=app
+
+
+app.use(router).use(store).use(ElementPlus).mount('#app')
